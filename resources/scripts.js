0f6d9256e0302d3d8ecb16e43efde6f7cb339d7d
Jørgen <100347162+Icyhuman@users.noreply.github.com>
2025-12-23 19:19:57 +0100

    Update scripts.js

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,10 +1,10 @@
 function recolor(event){ //get mouse pos, make color
   let x = event.clientX;
   let y = event.clientY;
-  var r = x/3;
-  var g = 255-x/3;
-  var b = y/3;
-  document.getElementById("dabox").style.background = rgb(r,g,b);
+  //var r = x/3;
+  //var g = 255-x/3;
+  //var b = y/3;
+  //document.getElementById("dabox").style.background = rgb(r,g,b);
   let coor = "Coordinates: (" + x + "," + y + ")";
   document.getElementById("coords").innerHTML=coor;
 }
