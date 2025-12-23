a22c8476ee657cbb19b8d70aade8b0ed162de286
Jørgen <100347162+Icyhuman@users.noreply.github.com>
2025-12-23 21:10:07 +0100

    rvla fuck gotta press some keys fast before copilot griefs me

--- resources/scripts.js
+++ resources/scripts.js
@@ -2,10 +2,8 @@ function recolor(event){ //get mouse pos, make color
   let x = event.clientX;
   let y = event.clientY;
   let r = x;
-  //let g = 255 - x;
-  //let b = y - 485;
-  //let rgebe="rgb("+r+","+g+","+b+")";
-  //document.getElementById("dabox").style.color = rgebe;
-  let coor = "Boordinates: (" + r + "," + y + ")";
-  document.getElementById("coords").innerHTML=coor;
+  let g = 255 - x;
+  let b = y - 485;
+  let rgebe="rgb("+r+","+g+","+b+")";
+  document.getElementById("dabox").style.background = rgebe;
 }
