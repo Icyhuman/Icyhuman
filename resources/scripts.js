38732b928f220f562b29f9d65fa6e65c30be40a3
Jørgen <100347162+Icyhuman@users.noreply.github.com>
2025-12-23 19:16:34 +0100

    Update scripts.js

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,8 +1,10 @@
-function recolor(){ //get mouse pos, make color
+function recolor(event){ //get mouse pos, make color
   let x = event.clientX;
   let y = event.clientY;
   var r = x/3;
   var g = 255-x/3;
   var b = y/3;
-  document.getElementById("dabox").style.background = rgb(r,g,b)"Uh oh silly you put in something other than numbers with commas and the code didn't like that";
+  document.getElementById("dabox").style.background = rgb(r,g,b);
+  let coor = "Coordinates: (" + x + "," + y + ")";
+  document.getElementById("coords").innerHTML=coor;
 }
