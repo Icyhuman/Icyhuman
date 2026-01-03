4e73365eacb8c75a41ac000689d1cc0a5dea2cc1
Jørgen <100347162+Icyhuman@users.noreply.github.com>
2026-01-03 15:53:04 +0100

    Update slideshow.js

--- resources/slideshow.js
+++ resources/slideshow.js
@@ -12,9 +12,10 @@ function switchslide(n) {
   let i;
   console.log("ushabti");
   let slides = document.getElementsByClassName("slide");
+  console.log(n);
+  console.log(slides.length);
   if (n >= slides.length) {index = 0}
-  if (n < 0) {index = slides.length}
-  console.log("funshabti");
+  if (n < 0) {index = slides.length-1}
   console.log(index);
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
