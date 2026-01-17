e4733ef28f241240bc8392aea98424e37d997e91
Jørgen <100347162+Icyhuman@users.noreply.github.com>
2026-01-17 20:01:47 +0100

    copied slideshow code with a 2 cause this is a silly lil site not a professional thing

--- resources/slideshow.js
+++ resources/slideshow.js
@@ -25,3 +25,31 @@ function switchslide(n) {
   console.log(slides[index].style.display);
   console.log("endshabti");
 } 
+
+var index2 = 0;
+console.log("ushab2");
+switchslide2(index2);
+
+function arrowCTRLS2(n) {
+  console.log("ushabti");
+  switchslide2(index2 += n);
+  console.log("ushabti2");
+}
+
+function switchslide2(n) {
+  let i;
+  console.log("ushabti");
+  let slides = document.getElementsByClassName("slide2");
+  console.log(n);
+  console.log(slides.length);
+  if (n >= slides.length) {index2 = 0}
+  if (n < 0) {index2 = slides.length-1}
+  console.log(index2);
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  console.log(slides[index2].style.display);
+  slides[index2].style.display = "block";
+  console.log(slides[index2].style.display);
+  console.log("endshabti");
+} 
