7b7afbf1af32a7c088105efe6901576fd0370820
Icyhuman <jorgen.storum@gmail.com>
2026-02-25 01:10:09 +0100

    Made the slideshow js generic, and much cleaner. Very verbose console cause I felt like it lol

--- resources/slideshow.js
+++ resources/slideshow.js
@@ -1,55 +1,58 @@
-var index = 0;
-console.log("ushabti2");
-switchslide(index);
+/**
+ * Slideshow code. A "slideframe" <div> must contain "slide" <div>s, and <a>s of classes "prev" and "next"
+ */
+//initial setup
+console.log("setting up slideshows");
+let sframes=document.getElementsByClassName("slideframe");
+for (i = 0; i < sframes.length; i++) {
+    sframes[i].style.setProperty('--index', 0);
+    switchslide(sframes[i],0)
+  }
 
-function arrowCTRLS(n) {
-  console.log("ushabti");
-  switchslide(index += n);
-  console.log("ushabti2");
+/**
+ * Arrow controls for slideshow. Arrows must have onclick="arrowCTRLS(this,n), where n is -1 or 1.
+ * @param  {div} arrow arrow that was clicked
+ * @param  {int} move -1 or 1, depending on slide direction
+ * @return {none}
+ */
+function arrowCTRLS(arrow, move) {
+  //console.log("ushabti");
+  let frame=arrow.parentNode;
+  console.log("switching slides in frame");
+  console.log(frame);
+  switchslide(frame,move);
 }
 
-function switchslide(n) {
+/**
+ * Slide switching code for slideshows.
+ * @param  {div} frame the slideframe in which slides are to be changed
+ * @param  {int} move -1 or 1, depending on slide direction
+ * @return {none}
+ */
+function switchslide(frame, move) {
   let i;
-  console.log("ushabti");
-  let slides = document.getElementsByClassName("slide");
-  console.log(n);
-  console.log(slides.length);
-  if (n >= slides.length) {index = 0}
-  if (n < 0) {index = slides.length-1}
+  //css didn't wanna store a number so I'm casting it
+  let index = Number(getComputedStyle(frame).getPropertyValue('--index'));
+  let slides = frame.getElementsByClassName("slide");
+  index+=move;
+  console.log("Target slide")
   console.log(index);
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  console.log(slides[index].style.display);
-  slides[index].style.display = "block";
-  console.log(slides[index].style.display);
-  console.log("endshabti");
-} 
-
-var index2 = 0;
-console.log("ushab2");
-switchslide2(index2);
-
-function arrowCTRLS2(n) {
-  console.log("ushabti");
-  switchslide2(index2 += n);
-  console.log("ushabti2");
-}
-
-function switchslide2(n) {
-  let i;
-  console.log("ushabti");
-  let slides = document.getElementsByClassName("slide2");
-  console.log(n);
+  console.log("Total slides")
   console.log(slides.length);
-  if (n >= slides.length) {index2 = 0}
-  if (n < 0) {index2 = slides.length-1}
-  console.log(index2);
+  //looping if target index is out of bounds
+  if (index >= slides.length) {index = 0;
+    console.log("Looped")
+  }
+  if (index < 0) {index = slides.length-1;
+    console.log("Looped")
+  }
+  //Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  console.log(slides[index2].style.display);
-  slides[index2].style.display = "block";
-  console.log(slides[index2].style.display);
-  console.log("endshabti");
+  //Unhide target slide
+  slides[index].style.display = "block";
+  frame.style.setProperty('--index', index);
+  console.log("yay the code didn't crash");
 } 
+
